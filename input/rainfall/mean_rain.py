0e8cfdf6feb037219e9efd0852ea73cb9db5bfe3
hasitha <hasithadkr7@gmail.com>
2019-08-31 13:44:00 +0530

    mean rain

--- input/rainfall/mean_rain.py
+++ input/rainfall/mean_rain.py
@@ -1,25 +1,23 @@
 import os
-import tempfile
-import unittest
-import numpy as np
 import geopandas as gpd
+import numpy as np
 from scipy.spatial import Voronoi
 from shapely.geometry import Polygon, Point
+from db_layer import CurwSimAdapter
 from resources import manager as res_mgr
+from config import MYSQL_USER, MYSQL_DB, MYSQL_HOST, MYSQL_PASSWORD
 
 
 def _voronoi_finite_polygons_2d(vor, radius=None):
     """
     Reconstruct infinite voronoi regions in a 2D diagram to finite
     regions.
-
     Parameters
     ----------
     vor : Voronoi
         Input diagram
     radius : float, optional
         Distance to 'points at infinity'.
-
     Returns
     -------
     regions : list of tuples
@@ -30,19 +28,15 @@ def _voronoi_finite_polygons_2d(vor, radius=None):
         end.
 
     from: https://stackoverflow.com/questions/20515554/colorize-voronoi-diagram
-
     """
-
     if vor.points.shape[1] != 2:
         raise ValueError("Requires 2D input")
 
     new_regions = []
     new_vertices = vor.vertices.tolist()
-
     center = vor.points.mean(axis=0)
     if radius is None:
         radius = vor.points.ptp().max()
-
     # Construct a map containing all ridges for a given point
     all_ridges = {}
     for (p1, p2), (v1, v2) in zip(vor.ridge_points, vor.ridge_vertices):
@@ -52,12 +46,10 @@ def _voronoi_finite_polygons_2d(vor, radius=None):
     # Reconstruct infinite regions
     for p1, region in enumerate(vor.point_region):
         vertices = vor.regions[region]
-
         if all(v >= 0 for v in vertices):
             # finite region
             new_regions.append(vertices)
             continue
-
         # reconstruct a non-finite region
         ridges = all_ridges[p1]
         new_region = [v for v in vertices if v >= 0]
@@ -68,9 +60,7 @@ def _voronoi_finite_polygons_2d(vor, radius=None):
             if v1 >= 0:
                 # finite ridge: already in the region
                 continue
-
             # Compute the missing endpoint of an infinite ridge
-
             t = vor.points[p2] - vor.points[p1]  # tangent
             t /= np.linalg.norm(t)
             n = np.array([-t[1], t[0]])  # normal
@@ -78,7 +68,6 @@ def _voronoi_finite_polygons_2d(vor, radius=None):
             midpoint = vor.points[[p1, p2]].mean(axis=0)
             direction = np.sign(np.dot(midpoint - center, n)) * n
             far_point = vor.vertices[v2] + direction * radius
-
             new_region.append(len(new_vertices))
             new_vertices.append(far_point.tolist())
 
@@ -87,10 +76,8 @@ def _voronoi_finite_polygons_2d(vor, radius=None):
         c = vs.mean(axis=0)
         angles = np.arctan2(vs[:, 1] - c[1], vs[:, 0] - c[0])
         new_region = np.array(new_region)[np.argsort(angles)]
-
         # finish
         new_regions.append(new_region.tolist())
-
     return new_regions, np.asarray(new_vertices)
 
 
@@ -126,94 +113,37 @@ def get_voronoi_polygons(points_dict, shape_file, shape_attribute=None, output_s
             data.append({'id': ids[i], 'lon': vor.points[i][0], 'lat': vor.points[i][1], 'area': intersection.area,
                          'geometry': intersection
                          })
-
     df = gpd.GeoDataFrame(data, columns=['id', 'lon', 'lat', 'area', 'geometry'], crs=shape_df.crs)
     return df
 
 
-def is_inside_geo_df(geo_df, lon, lat, polygon_attr='geometry', return_attr='id'):
-    point = Point(lon, lat)
-    for i, poly in enumerate(geo_df[polygon_attr]):
-        if point.within(poly):
-            return geo_df[return_attr][i]
-    return None
+def get_thessian_polygon_from_gage_points(output_dir, shape_file, gage_points):
+    shape = res_mgr.get_resource_path(shape_file)
+    # calculate the voronoi/thesian polygons w.r.t given station points.
+    voronoi_polygon = get_voronoi_polygons(gage_points, shape, ['OBJECTID', 1],
+                                       output_shape_file=os.path.join(output_dir, 'shape', 'sub_catchment.shp'))
+    print('voronoi_polygon : ', voronoi_polygon)
+    return voronoi_polygon
 
 
-class TestSpatialUtils(unittest.TestCase):
-    def test_get_voronoi_polygons(self):
-        points = {
-            'Colombo': [79.8653, 6.898158],
-            'IBATTARA3': [79.86, 6.89],
-            'Isurupaya': [79.92, 6.89],
-            'Borella': [79.86, 6.93, ],
-            'Kompannaveediya': [79.85, 6.92],
-        }
+def get_mean_rain(ts_start, ts_end, output_dir, catchment='kub'):
+    print('[ts_start, ts_end, output_dir, catchment] : ', [ts_start, ts_end, output_dir, catchment])
+    sim_adapter = CurwSimAdapter(MYSQL_USER, MYSQL_PASSWORD, MYSQL_HOST, MYSQL_DB)
+    if catchment == 'kub':
+        shape_file = res_mgr.get_resource_path('kub-wgs84/kub-wgs84.shp')
+    else:
+        shape_file = res_mgr.get_resource_path('klb-wgs84/klb-wgs84.shp')
+    # {station1:{'hash_id': hash_id1, 'latitude': latitude1, 'longitude': longitude1, 'timeseries': timeseries1}}
+    available_stations = sim_adapter.get_basin_available_stations_timeseries(shape_file, ts_start, ts_end)
+    # {'id' --> [lon, lat]}
+    gauge_points = {}
+    for station, info in available_stations.items():
+        gauge_points[station] = [info['longitude'], info['latitude']]
+    print('gauge_points : ', gauge_points)
+    gauge_points_thessian = get_thessian_polygon_from_gage_points(output_dir, shape_file, gauge_points)
+    print('gauge_points_thessian : ', gauge_points_thessian)
+    shape = res_mgr.get_resource_path('sub_catchments/sub_catchments.shp')
+    catchment_df = gpd.GeoDataFrame.from_file(shape)
+    print('catchment_df : ', catchment_df)
+    sim_adapter.close_connection()
 
-        shp = res_mgr.get_resource_path('extraction/shp/klb-wgs84/klb-wgs84.shp')
-        out = tempfile.mkdtemp(prefix='voronoi_')
-        result = get_voronoi_polygons(points, shp, ['OBJECTID', 1], output_shape_file=os.path.join(out, 'out.shp'))
-        print(result)
-
-    def test_is_inside_polygon(self):
-        points = {
-            'Colombo': [79.8653, 6.898158],
-            'IBATTARA3': [79.86, 6.89],
-            'Isurupaya': [79.92, 6.89],
-            'Borella': [79.86, 6.93, ],
-            'Kompannaveediya': [79.85, 6.92],
-        }
-
-        shp = res_mgr.get_resource_path('extraction/shp/klb-wgs84/klb-wgs84.shp')
-        result = get_voronoi_polygons(points, shp, ['OBJECTID', 1])
-        print(result)
-        for k in points.keys():
-            pp = is_inside_geo_df(result, points[k][0], points[k][1])
-            print(points[k], pp)
-            self.assertEqual(pp, k)
-
-    def test_get_voronoi_polygons_kub(self):
-        points = {
-            'Colombo': [79.8653, 6.898158],
-            'IBATTARA3': [79.86, 6.89],
-            'Isurupaya': [79.92, 6.89],
-            'Daraniyagala': [80.33805556, 6.924444444],
-            'Glencourse': [80.20305556, 6.978055556],
-            'Hanwella': [80.08166667, 6.909722222],
-            'Holombuwa': [80.26480556, 7.185166667],
-            'Kitulgala': [80.41777778, 6.989166667],
-            'Borella': [79.86, 6.93, ],
-            'Kompannaveediya': [79.85, 6.92],
-        }
-
-        shp = res_mgr.get_resource_path('extraction/shp/kelani-upper-basin.shp')
-        out = tempfile.mkdtemp(prefix='voronoi_')
-        print(out)
-        result = get_voronoi_polygons(points, shp, ['OBJECTID', 1], output_shape_file=os.path.join(out, 'out.shp'))
-        print(result)
-
-    def test_compare_voronoi_polygons(self):
-
-        files = ['up_in_3_thie_join.shp', 'up_in_11_thie_join.shp']
-        out = tempfile.mkdtemp(prefix='voronoi_')
-
-        for f in files:
-            voronoi_shp_file = res_mgr.get_resource_path('test/shp/%s' % f)
-            area_shp_file = res_mgr.get_resource_path('extraction/shp/kub-wgs84/kub-wgs84.shp')
-
-            shape_df = gpd.GeoDataFrame.from_file(voronoi_shp_file)
-
-            points = {}
-            for i in range(len(shape_df)):
-                points[shape_df['OBJECTID'][i]] = [shape_df['x'][i], shape_df['y'][i]]
-
-            result = get_voronoi_polygons(points, area_shp_file, ['OBJECTID', 1],
-                                          output_shape_file=os.path.join(out, '%s_out.shp' % f))
-
-            for i in range(len(shape_df)):
-                self.assertAlmostEqual(result['area'][i], shape_df['Shape_Area'][i], places=4)
-
-
-def suite():
-    s = unittest.TestSuite()
-    s.addTest(TestSpatialUtils)
-    return s
