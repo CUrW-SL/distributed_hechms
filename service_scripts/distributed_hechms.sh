ada028d5fbb505bee528085798b4c70bacf38932
hasitha <hasithadkr7@gmail.com>
2019-09-04 03:06:21 +0530

    bash script for starting distributed hechms server

---
+++ service_scripts/distributed_hechms.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Print execution date time
+echo `date`
+
+# Change directory into where distributed hechms implementation is located.
+echo "Changing into /home/uwcc-admin/distributed_hec/distributed_hechms"
+cd /home/uwcc-admin/distributed_hec/distributed_hechms
+echo "Inside `pwd`"
+
+# Activate venv.
+echo "Activating venv python3 virtual environment."
+source venv/bin/activate
+
+python3 controller.py >> distributed_hechms.log 2>&1
+
+# Deactivating virtual environment
+echo "Deactivating virtual environment"
+deactivate
