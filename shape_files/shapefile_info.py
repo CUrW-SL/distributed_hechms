fe7db65c2771fbfe044e88f38c71608b72ce589c
hasitha <hasithadkr7@gmail.com>
2019-09-12 07:09:10 +0530

    shape file info extraction

--- shape_files/shapefile_info.py
+++ shape_files/shapefile_info.py
@@ -9,5 +9,8 @@ if __name__ == '__main__':
     shape_polygon = shape_df['geometry']
     print('shape_polygon : ', shape_df['geometry'])
     area = shape_polygon.area
-    print('area : ', area)
-
+    print('kub area : ', area)
+    sub_shape_file = res_mgr.get_resource_path('sub_catchments/sub_subcatchments.shp')
+    catchment_df = gpd.GeoDataFrame.from_file(sub_shape_file)
+    for i, catchment_polygon in enumerate(catchment_df['geometry']):
+        print('sub area : ', catchment_polygon.area)
