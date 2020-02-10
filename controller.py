26761c49b4076b52a29a0761c0530dc2bbf424db
hasitha <hasithadkr7@gmail.com>
2020-02-10 13:18:29 +0530

    controller update

--- controller.py
+++ controller.py
@@ -32,9 +32,6 @@ app.config['UPLOADS_DEFAULT_DEST'] = path.join(UPLOADS_DEFAULT_DEST, 'FLO2D')
 app.config['UPLOADED_FILES_ALLOW'] = ['csv', 'run', 'control']
 
 # upload set creation
-model_distributed = UploadSet('configfiles', extensions=('csv','run','control'))
-
-configure_uploads(app, model_distributed)
 flask_json.init_app(app)
 
 
@@ -47,50 +44,6 @@ def create_dir_if_not_exists(path):
 @app.route('/')
 def hello_world():
     return 'Welcome to HecHms(Distributed) Server!'
-
-
-@app.route('/HECHMS/distributed/init-run', methods=['POST'])
-def init_run():
-    req_args = request.args.to_dict()
-    # Check whether run-name is specified and valid.
-    if 'run-name' not in req_args.keys() or not req_args['run-name']:
-        raise JsonError(status_=400, description='run-name is not specified.')
-    run_name = req_args['run-name']
-    if not is_valid_run_name(run_name):
-        raise JsonError(status_=400, description='run-name cannot contain spaces or colons.')
-    # Valid base-dt must be specified at the initialization phase
-    if 'base-dt' not in req_args.keys() or not req_args['base-dt']:
-        raise JsonError(status_=400, description='base-dt is not specified.')
-    base_dt = req_args['base-dt']
-    if not is_valid_init_dt(base_dt):
-        raise JsonError(status_=400, description='Given base-dt is not in the correct format: %s'
-                                                 % INIT_DATE_TIME_FORMAT)
-    # Valid run-dt must be specified at the initialization phase
-    if 'run-dt' not in req_args.keys() or not req_args['run-dt']:
-        raise JsonError(status_=400, description='run-dt is not specified.')
-    run_dt = req_args['run-dt']
-    if not is_valid_init_dt(run_dt):
-        raise JsonError(status_=400, description='Given run-dt is not in the correct format: %s'
-                                                 % INIT_DATE_TIME_FORMAT)
-
-    today = datetime.today().strftime('%Y-%m-%d')
-    input_dir_rel_path = path.join(today, run_name, 'input')
-    # Check whether the given run-name is already taken for today.
-    input_dir_abs_path = path.join(UPLOADS_DEFAULT_DEST, 'HECHMS', 'distributed', input_dir_rel_path)
-    if path.exists(input_dir_abs_path):
-        raise JsonError(status_=400, description='run-name: %s is already taken for today: %s.' % (run_name, today))
-
-    req_files = request.files
-    if 'inflow' in req_files and 'outflow' in req_files and 'raincell' in req_files:
-        model_distributed.save(req_files['rainfall'], folder=input_dir_rel_path, name='daily_rain.csv')
-        model_distributed.save(req_files['model_run'], folder=input_dir_rel_path, name='model.run')
-        model_distributed.save(req_files['model_control'], folder=input_dir_rel_path, name='model.control')
-        model_distributed.save(req_files['model_gage'], folder=input_dir_rel_path, name='model.gage')
-    else:
-        raise JsonError(status_=400, description='Missing required input files. Required inflow, outflow, raincell.')
-
-    run_id = 'FLO2D:model250m:%s:%s' % (today, run_name)  # TODO save run_id in a DB with the status
-    return json_response(status_=200, run_id=run_id, description='Successfully saved files.')
 
 
 @app.route('/HECHMS/distributed/init', methods=['GET', 'POST'])
